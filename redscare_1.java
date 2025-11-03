58b183328b2b2cb64011fb5789e8591e636dfc1a
Marius Thomsen <mariu@itu.dk>
2025-11-03 12:30:28 +0100

    read data

--- redscare_1.java
+++ redscare_1.java
@@ -3,18 +3,46 @@ package redscare;
 import java.util.*;
 
 public class redscare_1 {
+    static int n;
+    static int s;
+    static int t;
+    static Set<Integer> r;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int s = sc.nextInt();
-        int t = sc.nextInt();
 
-        System.out.println(n);
-        System.out.println(s);
-        System.out.println(t);
-        for (int i = 0; i < n; i++) {
+        List<List<Integer>> adj = readAdjMatrix(sc);
 
+        System.out.println("N: " + n + " s:" + s + " t:" + t);
+        System.out.println("R: " + r);
+        System.out.println("adf: " + adj);
+    }
+
+    private static List<List<Integer>> readAdjMatrix(Scanner sc) {
+        n = sc.nextInt();
+        s = sc.nextInt();
+        t = sc.nextInt();
+        sc.nextLine();
+
+        String[] redlist = sc.nextLine().split(" ");
+        r = new HashSet<>();
+        if (!redlist[0].isEmpty()) {
+            for (String v : redlist) {
+                r.add(Integer.parseInt(v));
+            }
         }
 
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+            String[] edges = sc.nextLine().split(" ");
+            for (String e : edges) {
+                if (!e.isEmpty())
+                    adj.get(i).add(Integer.parseInt(e));
+            }
+        }
+
+        sc.close();
+        return adj;
     }
 }
